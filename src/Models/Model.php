04dc75e4773891875f9c93e3ac266660774c9372
Pablo Jonhatan Solis Mendiola <j161099@gmail.com>
2022-09-07 03:00:32 -0500

    Corrected Error in the namespace of the UsesUuidPrimaryKey trait

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -2,7 +2,7 @@
 
 namespace J1610\EloquentPrimaryUuids\Models;
 
-use App\Traits\UsesUuidPrimaryKey;
+use J1610\EloquentPrimaryUuids\Traits\UsesUuidPrimaryKey;
 use Illuminate\Database\Eloquent\Model as BaseModel;
 
 class Model extends BaseModel
